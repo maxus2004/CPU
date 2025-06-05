924698e2b779b654ee56ec94925ace96cf1ab62e
Maksim Doroshin <m1doroshin@email.com>
2025-06-05 21:09:46 +0300

    fix format

--- text_to_bin/text_to_bin.py
+++ text_to_bin/text_to_bin.py
@@ -7,8 +7,11 @@ with open("code.txt", "r") as text_file:
         line = line.replace(" ","")
         string += line
 
-with open("code.bin", "wb") as binary_file:
-    for i in range(len(string)//8):
-        byte_string = string[i*8 : i*8+8]
-        byte = int(byte_string,2).to_bytes()
-        binary_file.write(byte)
+with open("code0.bin", "wb") as binary_file0, open("code1.bin", "wb") as binary_file1:
+    for i in range(len(string)//16):
+        byte_string0 = string[i*16 : i*16+8]
+        byte0 = int(byte_string0,2).to_bytes()
+        binary_file0.write(byte0)
+        byte_string1 = string[i*16+8 : i*16+16]
+        byte1 = int(byte_string1,2).to_bytes()
+        binary_file1.write(byte1)
