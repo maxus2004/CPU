023981833868a1999b3bd278cea1643ed7746d23
Maksim Doroshin <m1doroshin@email.com>
2025-06-05 21:43:17 +0300

    swap bytes

--- text_to_bin/text_to_bin.py
+++ text_to_bin/text_to_bin.py
@@ -9,9 +9,9 @@ with open("code.txt", "r") as text_file:
 
 with open("code0.bin", "wb") as binary_file0, open("code1.bin", "wb") as binary_file1:
     for i in range(len(string)//16):
-        byte_string0 = string[i*16 : i*16+8]
-        byte0 = int(byte_string0,2).to_bytes()
-        binary_file0.write(byte0)
-        byte_string1 = string[i*16+8 : i*16+16]
+        byte_string1 = string[i*16 : i*16]
         byte1 = int(byte_string1,2).to_bytes()
         binary_file1.write(byte1)
+        byte_string0 = string[i*16+8 : i*16+16]
+        byte0 = int(byte_string0,2).to_bytes()
+        binary_file0.write(byte0)
